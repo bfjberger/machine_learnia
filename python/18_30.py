25534f440c05fba0c26e3ce2e8fcc534d62f92d9
Benoit Berger <bberger@c2r3s6.42lausanne.ch>
2023-12-01 13:00:08 +0100

    18

---
+++ python/18_30.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+bitcoin = pd.read_csv('BTC-EUR.csv', index_col='Date', parse_dates=True)
+# bitcoin['Close'].plot(figsize=(9, 6))
+# print(bitcoin.head())
+# bitcoin.loc['2019-09', 'Close'].plot()
+bitcoin.loc['2016':'2023', 'Close'].plot()
+plt.show()
+# print(bitcoin.index)
+# if we need to change the time format use : pd.to_datetime('20129/03/20')
+# timestamp('2019-03-20 00:00:00')
+
+
