857a712b16be594beab3bfab7f6bdb9f157f533e
Benoit Berger <bberger@student.42lausanne.ch>
2024-02-29 11:42:55 +0100

    14

--- python/14_30.py
+++ python/14_30.py
@@ -45,11 +45,11 @@
 # graphique(dataset)        
 # plt.show()
 
-import numpy as np
+# import numpy as np
 
-x = np.linspace(0, 2, 10)
-y = x**2
-print(x)
+# x = np.linspace(0, 2, 10)
+# y = x**2
+# print(x)
 
 # before plot , we have to make figure and define the size of it in inch
 # plt.figure(figsize=(12, 8))
@@ -64,12 +64,34 @@ print(x)
 # choose line thickness (epaisseur) lw=4 => plt.plot(x, y, c='red', lw=4)
 # choose line style ls='--' => plt.plot(x, y, c='red', lw=4, ls='--')
 
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
-plt.figure(figsize=(12, 8))
-plt.plot(x, y)
-plt.show()
+# plt.figure(figsize=(12, 8))
+# plt.plot(x, y)
+# plt.show()
 
 # plot several graph in the same fig using subplot
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+dataset = {f"experience{i}": np.random.randn(100) for i in range(4)}
+
+
+def graphique(dataset):
+    n = len(dataset)
+    plt.figure(figsize=(12, 8))
+    for k, i in zip(dataset.keys(), range(1, n+1)):
+        plt.subplot(n, 1, i)
+        plt.plot(dataset[k])
+        plt.title(k)
+        plt.xlabel('axe x')
+        plt.ylabel('axe y')
+
+graphique(dataset)
+plt.show()
+
+
+
 
 
